cd6093b77e9ce0086d94c8a1ca3d9142a1636a03
BeingLight <2033526984.qq.com>
2023-04-06 08:49:26 +0800

    ch01

---
+++ ch01/build/CMakeFiles/helloword.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloword.dir/helloword.cpp.o"
+  "CMakeFiles/helloword.dir/helloword.cpp.o.d"
+  "helloword"
+  "helloword.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloword.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
